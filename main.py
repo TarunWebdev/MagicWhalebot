f509b37469660f43568f909c6e3940ac19234738
Tarun Sharma <48819206+TarunWebdev@users.noreply.github.com>
2022-07-04 01:24:43 +0530

    done

--- main.py
+++ main.py
@@ -5,13 +5,8 @@ import time
 from Alive import Alive
 from threading import Timer
 
-prevlength = 0
-exponent18 = 10 ** -18
-amount = 0
-prevTotalSupply = 0
-currentTotalSupply = 0
-threshold = 1
-discordnoti  = 0
+last_time = [0]
+
 
 async def sendMessage(message):
   await discord.utils.get(client.get_all_channels(),name='general').send(message)
@@ -26,56 +21,31 @@ async def sectimer(message):
   while True:
     if message.content.startswith('stop'):
       return
-    await sendMessage(f'sleep attempt')
+    await sendMessage(getBuy(last_time))
     time.sleep(5)
   
-
-def totalSupply(data):
-  for i in range(0,len(data[0]['children'])):
-    for j in range(0,len(data[0]['children'][i])):
-      if "name" in data[0]['children'][i] and data[0]['children'][i]['name'] == "totalSupply":
-        return float(data[0]['children'][i]['value'])*exponent18
-  return 0
-
-def compareTotalSupply(prevTotalSupply, currentTotalSupply,threshold):
-  if abs(prevTotalSupply - currentTotalSupply) > threshold :
-    return currentTotalSupply - prevTotalSupply
-  else:
-    return 0
   
-def getBuy(prevlength):
-
-  #extracting hash and buy details
-  URL1="https://api.better-call.dev/v1/contract/hangzhou2net/KT1Mdk7TfHjbwb2ciTTsySj9gV9W9uc7HxCu/operations"
-  
+def getBuy(last_time):
+  if len(last_time)>5:
+    t = last_time[-1]
+    temp = [t]
+    last_time[:]=temp[:]
+  URL1="https://api.arbiscan.io/api?module=account&action=tokentx&contractaddress=0x539bdE0d7Dbd336b79148AA742883198BBF60342&page=1&offset=100&sort=desc&apikey=WMXSRXC98X444KH6PR1ZZNWARHH2RX8SBX"
   r = requests.get(url=URL1)
   data = r.json()
-  length = (len(data["operations"])) - prevlength
-  prevlength = len(data["operations"])
-  
-  amount = 0
-  for i in range(0,length) :
-    hash=data['operations'][i]['hash']
-    if "entrypoint" in data['operations'][i] and data['operations'][i]['entrypoint'] == "buy":
-      amount = int(data['operations'][i-1]['parameters'][0]['children'][2]['value']) * exponent18
-      print("Hash : " + str(hash),'\n',"Amount : " +str(amount))
-
-def getTotalSupply():
-  #extracting totalSupply
-  URL2 = "https://api.better-call.dev/v1/contract/hangzhou2net/KT1K2PdgMbMuNSgTWYsTcoa6Du4KeJNeVC9U/storage"
-  
-  r = requests.get(url=URL2)
-  data = r.json()
-  supply = totalSupply(data)
-  print("Current totalSupply : ", supply)
-  return supply
-  
-getBuy(prevlength)
-
-currentTotalSupply = getTotalSupply()
-prevTotalSupply = currentTotalSupply
-
-compareTotalSupply(prevTotalSupply, currentTotalSupply,threshold)
+  results = data['result']
+  # print(last_time)
+  # print("++++++++++++++++++++++++")
+  temp=[]
+  for x in results:
+      if(int(x['value']) > 1 * 10**18)and(int(x['timeStamp'])>last_time[-1]):
+          temp.append(x['value']+x['hash'])
+  last_time.append(int(results[0]['timeStamp']))
+  print(last_time)
+  print("----------------")
+  return(temp)
+ 
+getBuy(last_time)
 
 client = discord.Client()
 
@@ -104,16 +74,5 @@ async def on_message(message):
 
 Alive()
 
-#bot token to be added
+# bot token to be added
  
-  
-  #temp = compareTotalSupply(prevTotalSupply, currentTotalSupply,threshold)
-  #prevTotalSupply = currentTotalSupply
-  # timestamp=data['operations'][i]['timestamp']   #URL2="https://api.hangzhou2net.tzkt.io/v1/operations/"+hash
-    # r2=requests.get(url=URL2)
-    # data2=r2.json()
-    # print(data2[i]['amount'])
-  # oozYCzfzu5Ri9FeXK6KUiMq5dsYCRQktpbtkgkyCocr5bbyxG8W
-
-  #if message.content.startswith(''):
-    # await message.channel.send(f 'The current price of (message.content) is:   (getCryptoPrices (message.content. Lower ())) USD')
